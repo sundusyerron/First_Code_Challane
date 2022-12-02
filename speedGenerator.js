e36116bbe8b95b4e4dc264c9af20ea16c6ead415
Sundus <sundusaden105@gmail.com>
2022-12-02 23:49:27 +0300

    completed challenge-3

--- speedGenerator.js
+++ speedGenerator.js
@@ -1,18 +1,26 @@
-function submit(){
-    let  SpeedOfCar =parseInt(document.getElementById("speedOfCar").value) ;
-    if (SpeedOfCar <= 70)
-        document.getElementById("status").innerHTML='OK'
-        
-     
-    else if (SpeedOfCar > 70)  {
-        const points = (SpeedOfCar - 70)/5
 
-        if (points >= 12)
-        document.getElementById("status").innerHTML='Licence Suspended'
-
-        }
-    
-    }
-
+//Challenge 2: Speed Detector (Toy Problem)
+//here we calculate speed of a car 
+//lets speed of a car is 80;
+const prompt = require('prompt-sync')();
+//get speed as input
+let speed =prompt("please enter speed");
+function speedOfAcar(){
+let speedOfAcar = 80;
+let demeritPointsOfCar = (speedOfAcar - 70)/5;
+//using if statemnt..if speedOfAcar is less than 70; we  print "Ok";
+if(speedOfAcar<70){
+    console.log("Ok");
+  }
+  //otherwise we prints points if points exceeds 12,we print liscences suspended..for every 5 km/hr above speed limit 70km/hr,
+  else {
+    console.log("demeritPoint:" + demeritPointsOfCar);
+  //if the points exceeds 12 we print licences suspended
+   if (demeritPointsOfCar > 12 ){
+  console.log("licence suspended");
+  }
+}
+}
+speedOfAcar();
 
 
